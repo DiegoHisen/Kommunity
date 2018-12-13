3aea79a488c55385fb714bf920cc2c9eb82774ee
Unknown <diego.hisen@gmail.com>
2018-12-13 10:57:51 +0100

    Kommunity_RazorApp

---
+++ System/KommunityWebApp/KommunityRazorApp/Kommunity_WebApp/Kommunity_WebApp/Models/Petition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kommunity_WebApp.Models
+{
+    public class Petition
+    {
+        public int id { get; set; }
+        public long creatorCPR { get; set; }
+        public string content { get; set; }
+        public string date { get; set; }
+        public bool approved { get; set; }
+        public long approvedby { get; set; }
+        public int numOfVotes { get; set; }
+    }
+}
---
+++ System/KommunityWebApp/KommunityRazorApp/Kommunity_WebApp/Kommunity_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Kommunity_WebApp.Services;
+using Kommunity_WebApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kommunity_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public LoginData loginData { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                Models.User user = new Models.User();
+                user.cpr = loginData.cpr;
+                user.password = loginData.Password;
+
+                var result = Service.findUser(user);
+                if (result.Equals(null))
+                {
+                    ModelState.AddModelError("", "username or password is invalid");
+                    return Page();
+                }
+                bool isValid = (loginData.cpr == result.cpr && loginData.Password == result.password);
+                if (!isValid)
+                {
+                    ModelState.AddModelError("", "username or password is invalid");
+                    return Page();
+                }
+
+                //Create Identity from user info
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, loginData.cpr));
+                identity.AddClaim(new Claim(ClaimTypes.Name, loginData.cpr));
+                //Authenticate using the identity
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = loginData.RememberMe });
+                return RedirectToPage("user/userindex");
+            }
+            else
+            {
+                ModelState.AddModelError("", "username or password is blank");
+                return Page();
+            }
+        }
+
+        public class LoginData
+        {
+            [Required]
+            public string cpr { get; set; }
+
+            [Required, DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+        }
+    }
+}
